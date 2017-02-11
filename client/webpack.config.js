cd84cc974f8a045bf699b057d4dc5741ff4bde54
Hung Tran <hung@0x0.ninja>
2017-02-10 22:31:49 -0600

    update front-end dependencies

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,58 +1,96 @@
 const path = require('path')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-const BUILD_DIR = path.resolve(__dirname, 'web')
 const APP_DIR = path.resolve(__dirname, 'app')
+const BUILD_DIR = path.resolve(__dirname, 'web')
 
 const config = {
+  devtool: 'source-map',
   entry: `${APP_DIR}/index.jsx`,
   output: {
     path: BUILD_DIR,
-    filename: 'js/web.js',
+    filename: 'js/bundle.js',
   },
   resolve: {
-    extensions: ['', '.js', '.jsx', '.scss'],
-    modulesDirectories: ['node_modules'],
+    extensions: ['.js', '.jsx', '.scss'],
+    modules: ['node_modules'],
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.jsx?$/,
         include: APP_DIR,
-        loader: 'babel',
+        loader: 'babel-loader',
       },
       {
         test: /\.(scss|css)$/,
-        loader: ExtractTextPlugin.extract('style', 'css!sass'),
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                includePaths: [path.resolve(__dirname, 'node_modules')],
+              }
+            },
+          ]
+        }),
       },
       {
         test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/font-woff&name=/fonts/[name].[ext]',
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          mimetype: 'application/font-woff',
+          name: '/fonts/[name].[ext]',
+        },
       },
       {
         test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/font-woff&name=/fonts/[name].[ext]',
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          mimetype: 'application/font-woff',
+          name: '/fonts/[name].[ext]',
+        },
       },
       {
         test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/octet-stream&name=/fonts/[name].[ext]',
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          mimetype: 'application/octet-stream',
+          name: '/fonts/[name].[ext]',
+        },
       },
       {
         test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/vnd.ms-fontobject&name=/fonts/[name].[ext]',
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          mimetype: 'application/vnd.ms-fontobject',
+          name: '/fonts/[name].[ext]',
+        },
       },
       {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=image/svg+xml&name=/fonts/[name].[ext]',
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          mimetype: 'image/svg+xml',
+          name: '/fonts/[name].[ext]',
+        },
       },
     ],
   },
   plugins: [
-    new ExtractTextPlugin('css/web.css'),
+    new ExtractTextPlugin({
+      filename: 'css/bundle.css',
+    }),
   ],
-  sassLoader: {
-    includePaths: [path.resolve(__dirname, 'node_modules')],
-  },
 };
 
 module.exports = config;
