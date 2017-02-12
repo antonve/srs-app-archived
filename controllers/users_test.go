1670eafbbc9e5a1e559a948edec2e6c83a022de5
Anton Van Eechaute <antonve@users.noreply.github.com>
2017-02-13 01:21:01 +0900

    Fix controller test due new echo version

--- controllers/users_test.go
+++ controllers/users_test.go
@@ -34,7 +34,7 @@ func TestCreateUser(t *testing.T) {
 	}
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec := httptest.NewRecorder()
-	c := e.NewContext(standard.NewRequest(req, e.Logger()), standard.NewResponse(rec, e.Logger()))
+	c := e.NewContext(req, rec)
 
 	if assert.NoError(t, controllers.APIUserRegister(c)) {
 		assert.Equal(t, http.StatusCreated, rec.Code)
@@ -57,7 +57,7 @@ func TestLoginUser(t *testing.T) {
 	}
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec := httptest.NewRecorder()
-	c := e.NewContext(standard.NewRequest(req, e.Logger()), standard.NewResponse(rec, e.Logger()))
+	c := e.NewContext(req, rec)
 
 	if assert.NoError(t, controllers.APIUserLogin(c)) {
 		// Check login response
