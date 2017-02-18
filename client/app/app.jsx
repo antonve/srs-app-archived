2e43bbdaf0c3df6a81c1bf3f60a31a9fb220b6f4
blindbox <blindcan@gmail.com>
2017-02-19 07:23:30 +0800

    Stricter eslint rules and minor code beautification.

--- client/app/app.jsx
+++ client/app/app.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
-import routes from './common/routes'
+import routes from './common/routes';
 import configureStore from './common/store/configure_store';
 
 const store = configureStore();
@@ -10,5 +10,5 @@ const App = () => (
   <Provider store={store}>
     <Router history={browserHistory} routes={routes} />
   </Provider>
-)
+);
 export default App;
