4b06e46bd08b45c01884a67183c08a0d02affe2a
antonve <mail@antonve.be>
2017-02-25 01:27:06 +0900

    Fix routing

--- app/app.jsx
+++ app/app.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { Router, browserHistory } from 'react-router'
+import { Router, hashHistory } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
 import routes from './common/routes'
 import configureStore from './common/store/configure_store'
 
 const store = configureStore()
+const history = syncHistoryWithStore(hashHistory, store)
 
 const App = () => (
   <Provider store={store}>
-    <Router history={browserHistory} routes={routes}/>
+    <Router history={history} routes={routes}/>
   </Provider>
 )
 
