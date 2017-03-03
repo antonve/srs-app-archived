164664b27b92e7daaebefd3444b75a47eb6588ad
antonve <mail@antonve.be>
2017-03-04 03:04:10 +0900

    Remove deck code for this PR

--- app/common/routes.jsx
+++ app/common/routes.jsx
@@ -4,13 +4,13 @@ import { Route, IndexRoute } from 'react-router'
 import AppLayout from './layouts/app'
 import requireAuthentication from './../auth/components/protected_route'
 
-import DecksOverview from './../srs/views/decks_overview'
+import HelloWorld from './../srs/views/HelloWorld'
 import AuthLogin from './../auth/views/login'
 import AuthRegister from './../auth/views/register'
 
 export default (
   <Route path="/" component={AppLayout}>
-    <IndexRoute component={DecksOverview}/>
+    <IndexRoute component={HelloWorld}/>
     {'/* Auth */'}
     <Route path="login" component={AuthLogin}/>
     <Route path="register" component={AuthRegister}/>
