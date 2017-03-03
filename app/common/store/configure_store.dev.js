42e0339a1375dc0d63cd2c6bd9190af73ab98ef4
antonve <mail@antonve.be>
2017-03-04 04:15:16 +0900

    Add redux dev tools

--- app/common/store/configure_store.dev.js
+++ app/common/store/configure_store.dev.js
@@ -3,13 +3,13 @@ import promise from 'redux-promise'
 import createLogger from 'redux-logger'
 import thunk from 'redux-thunk'
 import reducer from '../reducers'
-
+import DevTools from '../containers/DevTools'
 
 function ConfigureStore(initialState) {
   const logger = createLogger()
   const finalCreateStore = compose(
     applyMiddleware(promise, logger, thunk),
-    window.devToolsExtension ? window.devToolsExtension() : f => f,
+    DevTools.instrument(),
   )(createStore)
 
   const store = finalCreateStore(reducer, initialState)
