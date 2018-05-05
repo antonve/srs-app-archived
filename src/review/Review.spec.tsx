8807c849996a1e40d1961df0f9128018a781e8ea
Anton Van Eechaute <anton@wantedly.com>
2018-05-05 16:27:40 +0900

    add spec for when card should be shown

--- src/review/Review.spec.tsx
+++ src/review/Review.spec.tsx
@@ -7,3 +7,10 @@ test('Review shows empty state', () => {
   let tree = component.toJSON()
   expect(tree).toBe('Nothing to review.')
 })
+
+test('Review shows card when one is given', () => {
+  const card = jest.fn()
+  const component = create(<Review card={card} />)
+  let tree = component.toJSON()
+  expect(tree).not.toBe('Nothing to review.')
+})
