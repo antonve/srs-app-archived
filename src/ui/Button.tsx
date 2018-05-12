3a156206c8adf14cc02c34558fea34497e6347e3
Anton Van Eechaute <anton@wantedly.com>
2018-05-13 02:38:14 +0900

    add button state styles

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -9,16 +9,39 @@ interface ButtonProps {
 }
 
 const StyledButton = styled.button`
+  font-size: 1em;
   padding: 15px;
   color: ${theme.colors.light};
   background-color: ${theme.colors.dark};
+  border-radius: 3px;
+  box-shadow: 0 3px 5px 0 ${rgba(theme.colors.darker, 0.05)};
+  border: none;
+  transition: all 0.2s ease-out;
 
-  &:hover {
+  &:hover,
+  &:focus {
+    outline: none;
     cursor: pointer;
-    background-color: ${rgba(theme.colors.dark, 0.7)};
+    color: ${theme.colors.lighter};
+    box-shadow: 0 4px 7px 0 ${rgba(theme.colors.darker, 0.15)};
+  }
+
+  &:active {
+    outline: none;
+    box-shadow: inset 0 3px 5px 0 ${rgba(theme.colors.darker, 0.05)};
+  }
+
+  &:disabled {
+    opacity: 0.5;
+    pointer-events: none;
+    cursor: not-allowed;
   }
 `
 
 export const Button: React.SFC<ButtonProps> = ({ label, onClickHandler }) => {
-  return <StyledButton onClick={onClickHandler}>{label}</StyledButton>
+  return (
+    <StyledButton disabled onClick={onClickHandler}>
+      {label}
+    </StyledButton>
+  )
 }
