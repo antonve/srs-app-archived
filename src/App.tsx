fac5deb58273a74abfbf206c3fd4d0b3251f32e2
Anton Van Eechaute <anton@wantedly.com>
2018-05-13 02:44:45 +0900

    setup app main view

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import * as React from 'react'
 import { theme, Button } from 'src/ui/components'
-import styled from 'styled-components'
+import styled, { injectGlobal } from 'styled-components'
+
+injectGlobal`
+  body {
+    margin: 0;
+  }
+`
 
 const StyledApp = styled.div`
   font-family: ${theme.fonts.body};
+  background-color: ${theme.colors.dark};
+  width: 100vw;
+  height: 100vh;
 `
 
 export class App extends React.Component<{}, {}> {
