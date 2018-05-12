61411a6b51ca14131fe7013ecef516db323a0261
Anton Van Eechaute <anton@wantedly.com>
2018-05-12 15:17:55 +0900

    add spec for ActionBar: init

---
+++ src/review/ActionBar.spec.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import { create } from 'react-test-renderer'
+
+import { ActionBar } from './ActionBar'
+import { ViewStates } from '../cards/CardRenderer'
+
+test('ActionBar shows `reveal state` button on init', () => {
+  const state = { time: 0, viewState: ViewStates.Front }
+  const component = create(<ActionBar reviewState={state} />)
+
+  let tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
