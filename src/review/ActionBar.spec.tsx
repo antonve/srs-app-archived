fe8f6d5ea537714f2f770c567ed257358e7b0c3e
Anton Van Eechaute <anton@wantedly.com>
2018-05-14 01:57:19 +0900

    refactor actionbar spec in preparation for enzyme

--- src/review/ActionBar.spec.tsx
+++ src/review/ActionBar.spec.tsx
@@ -4,18 +4,25 @@ import { create } from 'react-test-renderer'
 import { ActionBar } from 'src/review/ActionBar'
 import { ViewState } from 'src/model/interfaces'
 
-const actionBar = (viewState: ViewState) => {
-  return <ActionBar viewState={viewState} revealHandler={jest.fn()} gradeHandler={jest.fn()} />
-}
 test('ActionBar shows `reveal state` button on init', () => {
-  const component = create(actionBar(ViewState.Front))
+  const props = {
+    viewState: ViewState.Front,
+    revealHandler: jest.fn(),
+    gradeHandler: jest.fn(),
+  }
+  const component = <ActionBar {...props} />
 
-  let tree = component.toJSON()
+  let tree = create(component).toJSON()
   expect(tree).toMatchSnapshot()
 })
 
 test('ActionBar shows grading buttons on reveal', () => {
-  const component = create(actionBar(ViewState.Back))
+  const props = {
+    viewState: ViewState.Back,
+    revealHandler: jest.fn(),
+    gradeHandler: jest.fn(),
+  }
+  const component = create(<ActionBar {...props} />)
 
   let tree = component.toJSON()
   expect(tree).toMatchSnapshot()
