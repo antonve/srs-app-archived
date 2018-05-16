30888a05fea0d21ca3ead51d8a8dfdc4ee53c22c
Anton Van Eechaute <anton@wantedly.com>
2018-05-16 23:34:25 +0900

    add spec for grading

--- src/review/ReviewContainer.spec.tsx
+++ src/review/ReviewContainer.spec.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { shallow } from 'enzyme'
 
 import { ReviewContainer } from 'src/review/ReviewContainer'
-import { ViewState } from 'src/model/interfaces'
+import { ViewState, Grade } from 'src/model/interfaces'
 
 it('reveal changes the StateView to Back', () => {
   const wrapper = shallow(<ReviewContainer />)
@@ -11,4 +11,13 @@ it('reveal changes the StateView to Back', () => {
   expect(instance.state.reviewState.viewState).toBe(ViewState.Front)
   instance.reveal()
   expect(instance.state.reviewState.viewState).toBe(ViewState.Back)
+})
+
+it('grading changes card', () => {
+  const wrapper = shallow(<ReviewContainer />)
+  const instance: ReviewContainer = wrapper.instance() as ReviewContainer
+
+  const card = instance.state.card
+  instance.grade(Grade.Correct)
+  expect(instance.state.card).not.toEqual(card)
 })
