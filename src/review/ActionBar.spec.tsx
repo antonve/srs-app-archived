d40a0e68ace63b21fc962cebbc5c049eb9abbfda
Anton Van Eechaute <anton@wantedly.com>
2018-05-17 11:07:43 +0900

    refactor out props in actionbar spec

--- src/review/ActionBar.spec.tsx
+++ src/review/ActionBar.spec.tsx
@@ -6,50 +6,47 @@ import { ActionBar } from 'src/review/ActionBar'
 import { ViewState } from 'src/model/interfaces'
 import { Button } from 'src/ui/components'
 
-test('ActionBar shows `reveal state` button on init', () => {
-  const props = {
+describe('ActionBar', () => {
+  const initialProps = {
     viewState: ViewState.Front,
     revealHandler: jest.fn(),
     gradeHandler: jest.fn(),
   }
-  const component = <ActionBar {...props} />
 
-  let tree = create(component).toJSON()
-  expect(tree).toMatchSnapshot()
-})
+  test('shows `reveal state` button on init', () => {
+    const component = <ActionBar {...initialProps} />
 
-test('ActionBar shows grading buttons on reveal', () => {
-  const props = {
-    viewState: ViewState.Back,
-    revealHandler: jest.fn(),
-    gradeHandler: jest.fn(),
-  }
-  const component = create(<ActionBar {...props} />)
+    let tree = create(component).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
 
-  let tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
-})
+  test('shows grading buttons on reveal', () => {
+    const props = {
+      ...initialProps,
+      viewState: ViewState.Back,
+    }
+    const component = create(<ActionBar {...props} />)
 
-test('ActionBar triggers revealHandler after clicking reveal', () => {
-  const props = {
-    viewState: ViewState.Front,
-    revealHandler: jest.fn(),
-    gradeHandler: jest.fn(),
-  }
-  const tree = shallow(<ActionBar {...props} />)
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
 
-  tree.find(Button).simulate('click')
-  expect(props.revealHandler).toHaveBeenCalled()
-})
+  test('triggers revealHandler after clicking reveal', () => {
+    const props = initialProps
+    const tree = shallow(<ActionBar {...props} />)
 
-test('ActionBar triggers gradeHandler when clicking grading button', () => {
-  const props = {
-    viewState: ViewState.Back,
-    revealHandler: jest.fn(),
-    gradeHandler: jest.fn(),
-  }
-  const tree = shallow(<ActionBar {...props} />)
+    tree.find(Button).simulate('click')
+    expect(props.revealHandler).toHaveBeenCalled()
+  })
 
-  tree.find(Button).forEach(button => button.simulate('click'))
-  expect(props.gradeHandler).toHaveBeenCalledTimes(2)
+  test('triggers gradeHandler when clicking grading button', () => {
+    const props = {
+      ...initialProps,
+      viewState: ViewState.Back,
+    }
+    const tree = shallow(<ActionBar {...props} />)
+
+    tree.find(Button).forEach(button => button.simulate('click'))
+    expect(props.gradeHandler).toHaveBeenCalledTimes(2)
+  })
 })
