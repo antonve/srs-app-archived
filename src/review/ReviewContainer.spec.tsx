cf91132346d2782912b328d809596a5aeb099141
Anton Van Eechaute <anton@wantedly.com>
2018-05-17 11:04:35 +0900

    move into describe block

--- src/review/ReviewContainer.spec.tsx
+++ src/review/ReviewContainer.spec.tsx
@@ -4,20 +4,22 @@ import { shallow } from 'enzyme'
 import { ReviewContainer } from 'src/review/ReviewContainer'
 import { ViewState, Grade } from 'src/model/interfaces'
 
-it('reveal changes the StateView to Back', () => {
-  const wrapper = shallow(<ReviewContainer />)
-  const instance: ReviewContainer = wrapper.instance() as ReviewContainer
+describe('ReviewContainer', () => {
+  it('reveal changes the StateView to Back', () => {
+    const wrapper = shallow(<ReviewContainer />)
+    const instance: ReviewContainer = wrapper.instance() as ReviewContainer
 
-  expect(instance.state.reviewState.viewState).toBe(ViewState.Front)
-  instance.reveal()
-  expect(instance.state.reviewState.viewState).toBe(ViewState.Back)
-})
+    expect(instance.state.reviewState.viewState).toBe(ViewState.Front)
+    instance.reveal()
+    expect(instance.state.reviewState.viewState).toBe(ViewState.Back)
+  })
 
-it('grading changes card', () => {
-  const wrapper = shallow(<ReviewContainer />)
-  const instance: ReviewContainer = wrapper.instance() as ReviewContainer
+  it('grading changes card', () => {
+    const wrapper = shallow(<ReviewContainer />)
+    const instance: ReviewContainer = wrapper.instance() as ReviewContainer
 
-  const card = instance.state.card
-  instance.grade(Grade.Correct)
-  expect(instance.state.card).not.toEqual(card)
+    const card = instance.state.card
+    instance.grade(Grade.Correct)
+    expect(instance.state.card).not.toEqual(card)
+  })
 })
