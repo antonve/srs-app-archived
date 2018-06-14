9469fd44553cfce3212300a0eaafd46df40bacf1
Anton Van Eechaute <anton@wantedly.com>
2018-06-15 01:14:44 +0900

    implement first attempt at a template renderer in cardrenderer

--- src/cards/CardRenderer.tsx
+++ src/cards/CardRenderer.tsx
@@ -7,5 +7,23 @@ interface CardRendererProps {
 }
 
 export const CardRenderer: React.SFC<CardRendererProps> = ({ card, viewState }) => {
-  return <>{card.fields[viewState]}</>
+  let template: string = card.cardType.template[viewState]
+
+  if (template === null) {
+    return null
+  }
+
+  const replacer = (_: string, fieldName: string) => {
+    const field = card.cardType.fields.find(field => field.name === fieldName)
+
+    if (field === null) {
+      return `{{${fieldName}: field not found}}`
+    }
+
+    return card.fields[field.ID].value
+  }
+
+  const output = template.replace(/{{(.*?)}}/g, replacer)
+
+  return <>output</>
 }
